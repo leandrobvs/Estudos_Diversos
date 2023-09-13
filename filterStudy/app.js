5d5d5b4403419cbbac766a69e3a246afda6a9f78
Leandro <leandro@live.de>
2023-09-13 11:23:18 -0300

    update final

--- filterStudy/app.js
+++ filterStudy/app.js
@@ -47,3 +47,50 @@
     });
   };
 })();
+
+(function () {
+  let imgList = [];
+  let counter = 0;
+
+  const modal = document.querySelector('.modal-container');
+  const modalContent = document.querySelector('.modal-content');
+  const close = document.querySelector('.x');
+  const left = document.querySelector('.left');
+  const right = document.querySelector('.right');
+  const images = document.querySelectorAll('.img-store');
+
+  images.forEach(image => {
+    imgList.push(image.src);
+  });
+
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    counter = 0;
+  });
+
+  images.forEach(image => {
+    image.addEventListener('click', function (event) {
+      modal.style.display = 'block';
+
+      let src = event.target.src;
+      counter = imgList.indexOf(src);
+      modalContent.style.backgroundImage = `url(${src})`;
+    });
+  });
+
+  left.addEventListener('click', () => {
+    counter--;
+    if (counter < 0) {
+      counter = imgList.length - 1;
+    }
+    modalContent.style.backgroundImage = `url(${imgList[counter]})`;
+  });
+
+  right.addEventListener('click', () => {
+    counter++;
+    if (counter > imgList.length - 1) {
+      counter = 0;
+    }
+    modalContent.style.backgroundImage = `url(${imgList[counter]})`;
+  });
+})();
